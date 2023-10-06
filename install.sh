f02713882b42b99465e01b7afd93055778aac320
lkopik <81908442+lkopik@users.noreply.github.com>
2023-10-07 02:57:13 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
-#update sys
-sudo su
+#update
 sudo apt update -y
 sudo apt upgrade -y
 
 #install deafult
-apt install -y htop mc nmap openssh-server wine git
+apt install -y htop mc nmap openssh-server wine neofetch snap
+
+#not default(apt)
+apt install -y blender 
+#notdeafult(snap)
+snap install telegram-desktop pycharm-community --classic
+
+
+#exit
+exit
+
